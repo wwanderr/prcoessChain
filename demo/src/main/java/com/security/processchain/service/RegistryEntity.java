eae261270ecc6d04972ddab26f3e148ee48a5b6b
processChain <18395586550@163.com>
2025-10-21 17:13:08 +0800

    [add]增加注册表实体，并增加incluede值

---
+++ demo/src/main/java/com/security/processchain/service/RegistryEntity.java
@@ -0,0 +1,32 @@
+package com.security.processchain.service;
+
+/**
+ * 注册表实体
+ */
+public class RegistryEntity {
+    private String targetObject;
+    private String regValue;
+    
+    public RegistryEntity() {}
+    
+    public String getTargetObject() {
+        return targetObject;
+    }
+    
+    public void setTargetObject(String targetObject) {
+        this.targetObject = targetObject;
+    }
+    
+    public String getRegValue() {
+        return regValue;
+    }
+    
+    public void setRegValue(String regValue) {
+        this.regValue = regValue;
+    }
+}
+
+
+
+
+
